ef521cf90514f4be43855629f43c5fdaf1652436
unknown <ashmitgupta03@gmail.com>
2022-06-13 12:57:15 +0530

    Final commit

--- Serial communication/namo_dummy_for_ESP32/namo_dummy_for_ESP32.ino
+++ Serial communication/namo_dummy_for_ESP32/namo_dummy_for_ESP32.ino
@@ -1,4 +1,5 @@
 #include<WiFi.h>
+#include<String.h>
 #define RXp2 16
 #define TXp2 17
 
@@ -54,16 +55,25 @@ void loop(){
   WiFiClient client = server.available();   // Listen for incoming clients
 
   if (client) {                             // If a new client connects,
-     
+       digitalWrite(18,HIGH);
+       delay(1000);
+       digitalWrite(18,LOW);
+       String input = Serial2.readString();
+       String data="";
+       String data_1="";
+       for(int i =input.length()-5;i<input.length();i++)
+       {
+          data= data+input[i];
+       }
+       for(int i =input.length()-10;i<input.length()-5;i++)
+       {
+          data_1 = data_1+input[i];
+       }
     Serial.println("New Client.");          // print a message out in the serial port
     String currentLine = "";                // make a String to hold incoming data from the client
     while (client.connected()) {            // loop while the client's connected
       if (client.available()) {             // if there's bytes to read from the client,
 
-        digitalWrite(18,HIGH);
-        delay(1000);
-        digitalWrite(18,LOW);
-        String input = Serial2.readString();
         
         char c = client.read();             // read a byte, then
         Serial.write(c);                    // print it out the serial monitor
@@ -80,8 +90,16 @@ void loop(){
             client.println();
 
             // Display the HTML web page
-            client.print("<!DOCTYPE html><html>");
-            client.print("<head></head><body><p>"+input+"</p></body></html>");
+            client.print("<!DOCTYPE html><html lang=""en""><head><meta http-equiv=""Content-Type"" content=""text/html; charset=windows-1252""><title>Physical Condidion</title>");
+            client.print("<style>body{background-image:url(""https://wallpaperaccess.com/full/402525.jpg"");background-size:cover;background-attachment: fixed;}");
+            client.print(".wrapper{display:grid;grid-template-columns:1fr 2fr 2fr 4fr 1fr;grid-auto-rows:90px;}");          
+            client.print(".wrapper > div{border:1px black solid;}");
+            client.print("#RPCS{top:0;left:0;padding: 10px;position: fixed;color : transparent;background-image: url(""https://wallpaperaccess.com/full/402525.jpg"");-webkit-background-clip: text;background-size:cover;background-attachment: fixed;width:100%;text-align: center;vertical-align: text-top;font: bold small-caps 100px Georgia, serif;}");
+            client.print("#cover{top:0;left:0;padding: 10px;position:fixed;color : transparent;background-color:#000;width:100%;text-align: center;vertical-align: text-top;font: bold small-caps 100px Georgia, serif;opacity: 0.8;z-index: -1;}");
+            client.print("#card{border:none;border-radius: 25px;grid-column: 2/5;grid-row: 4/7;background-color: black;opacity: 0.8;z-index: 1;}");
+            client.print("#text{padding:30px;border:none;grid-column: 2/5;grid-row: 4/7;color : transparent;background-color:#ABABAB ;-webkit-background-clip: text;background-size:cover;background-attachment: fixed;font: bold small-caps 90px Georgia, serif;z-index: 2;}");
+            client.print("</style></head>""<body><div id=""RPCS"">Physical Conditions</div><div id =""cover"">Physical Conditions</div><div class=""wrapper""><div id=""card""></div><div id=""text"">Temperature :"+data_1+"&#186;C Humidity :"+data+"%</div>");
+            client.print("<div id = ""highlight""></div></div></body></html>");
             // The HTTP response ends with another blank line
             client.println();
             // Break out of the while loop
